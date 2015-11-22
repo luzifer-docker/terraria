bcde5916fa7ba7f8cc4fa0e2a53ac05dc118abb0
Knut Ahlers <knut@ahlers.me>
2015-11-22 13:57:22 +0100

    Fixed missing input for docker

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ if ! [ -e /data/config.ini ]; then
   cp ${SERVER}/config.ini.default /data/config.ini
 fi
 
-exec ${SERVER}/server/TerrariaServer.bin.x86_64 \
+exec tail -f /dev/null | ${SERVER}/server/TerrariaServer.bin.x86_64 \
   -config /data/config.ini
